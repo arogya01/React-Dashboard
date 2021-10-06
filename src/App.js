d119e9eb80fe74f6c6ea751d3eac92438fd0f266
arogya <arogyabichpuria16@gmail.com>
2021-10-06 16:30:06 +0530

    added all the routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { Topbar } from "./Components/Topbar/Topbar";
 import { Sidebar } from "./Components/Sidebar/Sidebar";
 import { Home } from "./Pages/Home/Home";
-import "./app.css";
-import { BrowserRouter as Router, Switch,  Route } from "react-router-dom";
 import {UserList} from './Pages/userList/UserList';
 import {Users} from './Pages/Users/Users';
+import {NewUser} from './Pages/NewUser/NewUser';
+import {ProductList} from './Pages/ProductList/ProductList';
+import {Product} from './Pages/Product/Product';
+import {NewProduct} from './Pages/NewProduct/NewProduct';
+import "./app.css";
+import { BrowserRouter as Router, Switch,  Route } from "react-router-dom";
 
 function App() {
   return (
@@ -22,6 +26,20 @@ function App() {
           <Route path="/user/:userId">
             <Users />
           </Route>
+          
+          <Route path="/newUser">
+            <NewUser />
+          </Route>
+          <Route path="/products">
+            <ProductList />
+          </Route>
+          <Route path="/product/:productId">
+            <Product />
+          </Route>
+          <Route path="/newproduct">
+            <NewProduct />
+          </Route>
+
         </Switch>
       </div>
     </Router>
